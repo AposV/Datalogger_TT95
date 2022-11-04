a36d570d67db3c040d341e2346c301b92f7645f9
AposV <vasileiou.apos@gmail.com>
2022-11-04 17:31:51 -0400

    Finished renaming script in datetime handlers

--- TT95_Helper/datetime_handlers.py
+++ TT95_Helper/datetime_handlers.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import datetime
-from os import walk
+import os
+from TT95_Helper import configurations
+
+names_config = configurations.naming_config
+
 
 def swap_datetime_format(date, inplace=False):
     import_format = "%d-%m-%Y %H:%M:%S"
@@ -15,12 +19,28 @@ def swap_datetime_format(date, inplace=False):
 
 
 def swap_df_datetime_format(df):
-    return df.Datetime.apply(lambda x: swap_datetime_format(x))
+    df.Datetime.apply(lambda x: swap_datetime_format(x, inplace=True))
+    return df
 
 
 def swap_dt_frmt_recursively(old_folder, new_folder):
-    for (dirpath, dirnames, filenames) in walk(old_folder):
-        f.extend(filenames)
-    print(f)
+    dir = os.listdir(old_folder)
+    names = None
+    for d in dir:
+        of = old_folder + '/' + d
+        nf = new_folder + '/' + d
+        if os.path.isfile(of) & ('csv' in of):
+            print('Reading: ' + of)
+            if 'arduino' in of:
+                names = names_config['raw_csv_columns']['arduino']
+            elif 'YP' in of:
+                names = names_config['raw_csv_columns']['yp']
+            elif 'libelium' in of:
+                names = names_config['raw_csv_columns']['libelium']
 
-
+            old_df = pd.read_csv(of, names=names)
+            new_df = swap_df_datetime_format(old_df)
+            new_df.to_csv(nf)
+            print('Writing: ' + nf)
+        else:
+            swap_dt_frmt_recursively(of, nf)
