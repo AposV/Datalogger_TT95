733ab29ed28a2e1e6f0155d5a3e74e127cf7363c
AposV <vasileiou.apos@gmail.com>
2022-11-07 14:25:33 -0500

    Minor bug fixes

--- upload_prev_day_data.py
+++ upload_prev_day_data.py
@@ -13,7 +13,7 @@ arduino_filepath = dth.get_prev_day_data_file('arduino')
 
 
 # Process Yieldpoint Sensors and clean data
-yp_data = pd.read_csv(yp_filepath, names=name_config['raw_csv_columns']['yp']).iloc[:10,:]
+yp_data = pd.read_csv(yp_filepath, names=name_config['raw_csv_columns']['yp']).iloc[1:5]
 print(yp_data['Inst_id'])
 
 # Separate data for each extensometer and the rock temp array
