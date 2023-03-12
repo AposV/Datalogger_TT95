4849c2a77fefdb5a5860d8a0a979fa865bb2f79c
AposV <vasileiou.apos@gmail.com>
2023-03-12 16:40:39 -0400

    Fixed improper datetime filtering for libelium data

--- upload_data_in_date_range.py
+++ upload_data_in_date_range.py
@@ -56,11 +56,12 @@ arduino_data = arduino_data.dropna()
 
 ext1_data = ext1_data[(ext1_data['Datetime'] >= start_date) & (ext1_data['Datetime'] <= stop_date)]
 ext2_data = ext2_data[(ext2_data['Datetime'] >= start_date) & (ext2_data['Datetime'] <= stop_date)]
+libelium_data = libelium_data[(libelium_data['Datetime'] >= start_date) & (libelium_data>=stop_date)]
 rta_data = rta_data[(rta_data['Datetime'] >= start_date) & (rta_data['Datetime'] <= stop_date)]
 arduino_data = arduino_data[(arduino_data['Datetime'] >= start_date) & (arduino_data['Datetime'] <= stop_date)]
 
 print('----------------')
-print(yp_data)
+print(ext1_data)
 print('----------------')
 print(libelium_data)
 print('----------------')
